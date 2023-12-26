f9920dedf34aa2910c0d2de0cb2d6d433dfc8efd
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2023-12-26 17:26:28 +0800

    test: change api

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,8 @@ import shutil
 from os import listdir, path, mkdir
 from typing import Iterator, Tuple, Callable, Any
 
-from avfcomp import AVFComp, AVFDecomp
+from avfcomp import AVFComp, AVFDecomp, CompType
+from avfcomp.basecomp import T_CompType
 
 work_dir = path.dirname(path.dirname(__file__))
 
@@ -19,11 +20,14 @@ exp_path = path.join(data_path, "avf_exp")
 cvf_path = path.join(data_path, "cvf")
 decomp_path = path.join(data_path, "avf_decomp")
 
+
 # refresh
-shutil.rmtree(cvf_path, ignore_errors=True)
-shutil.rmtree(decomp_path, ignore_errors=True)
-mkdir(cvf_path)
-mkdir(decomp_path)
+def refresh():
+    """Refresh the data directory."""
+    shutil.rmtree(cvf_path, ignore_errors=True)
+    shutil.rmtree(decomp_path, ignore_errors=True)
+    mkdir(cvf_path)
+    mkdir(decomp_path)
 
 
 def list_files(paths: str) -> Iterator[Tuple[str, str]]:
@@ -50,7 +54,7 @@ def cost_time(func: Callable) -> Callable[..., Tuple[Any, float]]:
 
 
 @cost_time
-def get_comp(paths: str) -> Tuple[int, int]:
+def get_comp(paths: str, method: T_CompType) -> Tuple[int, int]:
     """Compress all files."""
     rawsize = 0
     compsize = 0
@@ -59,36 +63,36 @@ def get_comp(paths: str) -> Tuple[int, int]:
         rawsize += path.getsize(file_path)
         cvf.process_in(file_path)
         comp = path.join(cvf_path, name.replace("avf", "cvf"))
-        cvf.process_out(comp)
+        cvf.process_out(comp, method)
         compsize += path.getsize(comp)
     return (compsize, rawsize)
 
 
 @cost_time
-def get_decomp(paths: str) -> int:
+def get_decomp(paths: str, method: T_CompType) -> int:
     """Decompress all files."""
     decompsize = 0
     cvf = AVFDecomp()
     for name, file_path in list_files(paths):
-        cvf.process_in(file_path)
+        cvf.process_in(file_path, method)
         decompsize += path.getsize(file_path)
         decomp = path.join(decomp_path, name.replace("cvf", "avf"))
         cvf.process_out(decomp)
     return decompsize
 
 
-def stat_comp(paths: str, mode: str = ""):
+def stat_comp(paths: str, method: T_CompType, mode: str = ""):
     """Get the statistics of compressed files."""
-    size, ctime = get_comp(paths)
+    size, ctime = get_comp(paths, method)
     compsize, rawsize = size
     ratio = 100 * (compsize / rawsize)
     speed = (rawsize / ctime) / 1024 / 1024
     print(f"{mode}: {ratio:.2f}% {speed:.2f} MB/s")
 
 
-def stat_decomp(paths: str):
+def stat_decomp(paths: str, method: T_CompType):
     """Get the statistics of decompressed files."""
-    size, dtime = get_decomp(paths)
+    size, dtime = get_decomp(paths, method)
     speed = (size / dtime) / 1024 / 1024
     print(f"{speed:.2f} MB/s")
 
@@ -102,19 +106,27 @@ class TestCompAndDecomp(unittest.TestCase):
             decomp = path.join(decomp_path, name)
             self.assertEqual(calc_file_hash(file_path), calc_file_hash(decomp))
 
-    def test_comp_and_decomp(self):
+    def comp_and_decomp(self, method: T_CompType):
         """Test compression and decompression."""
+        print("Refresh data directory: ")
+        refresh()
+
         print("Test compression: ")
-        stat_comp(beg_path, "beg")
-        stat_comp(int_path, "int")
-        stat_comp(exp_path, "exp")
+        stat_comp(beg_path, method, "beg")
+        stat_comp(int_path, method, "int")
+        stat_comp(exp_path, method, "exp")
 
         print("Test decompression: ")
-        stat_decomp(cvf_path)
+        stat_decomp(cvf_path, method)
 
         self.check_decomp(beg_path)
         self.check_decomp(int_path)
         self.check_decomp(exp_path)
+
+    def test_lzma(self):
+        """Test lzma compression and decompression."""
+        print("Test lzma compression and decompression: ")
+        self.comp_and_decomp(CompType.LZMA)
 
 
 if __name__ == "__main__":
